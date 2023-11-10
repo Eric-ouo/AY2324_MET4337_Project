98d59392e005d2bb8bc40cdeee31dd79305d6180
Eric-ouo <a812574@gmail.com>
2023-11-11 04:18:08 +0800

    feat: add PlayerMovement
    I've add easy ease, check if it's bad or not. Achieving the effect of "easy ease" which makes the movement of the player smoother and more natural

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask groundLayer;
+
+    private float horizontalMove;
+    private bool isJumping = false;
+    private bool isGrounded = false;
+    private Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        horizontalMove = Input.GetAxis("Horizontal") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(horizontalMove);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void MovePlayer(float horizontalMove)
+    {
+        Vector3 targetVelocity = new Vector2(horizontalMove, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
